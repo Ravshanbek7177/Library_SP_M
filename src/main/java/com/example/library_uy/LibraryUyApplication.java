db0ae20001ebde63660474c5c88a8456915f2a50
Ravshanbek <ahmedovravshanbek070@gmail.com>
2023-03-27 12:39:25 +0500

    prekt ochildi m

---
+++ src/main/java/com/example/library_uy/LibraryUyApplication.java
@@ -0,0 +1,14 @@
+package com.example.library_uy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryUyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryUyApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/com/example/library_uy/LibraryUyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.library_uy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryUyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
